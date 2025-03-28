cdbaf7dedfb1be3b0470e199d87f306ec255b1d5
lucjdo <33455403+lucjdo@users.noreply.github.com>
2025-03-28 18:47:12 -0300

    feat: Implement table pagination and filtering (#2)
    * Add product table pagination
    
    * Add product table filtering

--- src/app/components/product-table/product-table.component.ts
+++ src/app/components/product-table/product-table.component.ts
@@ -1,23 +1,44 @@
-import { Component, Input } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { Component, Input, ViewChild } from '@angular/core';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import {
+  MatPaginatorModule,
+  MatPaginator,
+  PageEvent,
+} from '@angular/material/paginator';
 import { DisplayedProduct } from '../../models/product-model';
 import { StockColorDirective } from '../../directives/stock-color.directive';
 import { DecimalPipe } from '@angular/common';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 type Currency = 'USD' | 'EUR';
 
 @Component({
   selector: 'app-product-table',
-  imports: [MatTableModule, StockColorDirective, DecimalPipe],
+  imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    StockColorDirective,
+    DecimalPipe,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './product-table.component.html',
   styleUrls: ['./product-table.component.scss'],
 })
 export class ProductTableComponent {
   @Input() products: DisplayedProduct[] = [];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   private currentCurrency: Currency = 'USD';
   private readonly EUR_RATE = 1.08;
+  pageSize = 10;
+  pageSizeOptions: number[] = [10, 25, 50, 100];
+  dataSource!: MatTableDataSource<DisplayedProduct>;
+  filterValue: string = '';
 
   constructor(private router: Router) {}
 
@@ -31,8 +52,33 @@ export class ProductTableComponent {
     'actions',
   ];
 
-  get dataSource() {
-    return this.products;
+  ngOnInit() {
+    this.dataSource = new MatTableDataSource<DisplayedProduct>(this.products);
+    this.dataSource.filterPredicate = (
+      data: DisplayedProduct,
+      filter: string
+    ) => {
+      return data.title.toLowerCase().includes(filter.toLowerCase());
+    };
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnChanges() {
+    if (this.dataSource) {
+      this.dataSource.data = this.products;
+    }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   getCurrencySymbol(): string {
