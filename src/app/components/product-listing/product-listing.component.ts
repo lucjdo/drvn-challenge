bbf0805051a28ed87c8e1d219fc5e26c73df9934
lucjdo <luciano.sejudo@gmail.com>
2025-03-28 15:16:10 -0300

    feat: create product table component

--- src/app/components/product-listing/product-listing.component.ts
+++ src/app/components/product-listing/product-listing.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductTableComponent } from '../product-table/product-table.component';
 
 @Component({
   selector: 'app-product-listing',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductTableComponent],
   templateUrl: './product-listing.component.html',
   styleUrls: ['./product-listing.component.scss'],
 })
