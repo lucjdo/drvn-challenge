6cda0acd13c76d1ad7d0d1f3ce6eeace03296315
lucjdo <luciano.sejudo@gmail.com>
2025-03-28 16:02:42 -0300

    feat: fetch products and list them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListingComponent } from './components/product-listing/product-listing.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductListingComponent],
+  imports: [RouterOutlet, ProductListingComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/product-listing/product-listing.component.ts
+++ src/app/components/product-listing/product-listing.component.ts
@@ -1,16 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductTableComponent } from '../product-table/product-table.component';
+import { ProductService } from '../../services/product.service';
+import { DisplayedProduct } from '../../models/product-model';
 
 @Component({
   selector: 'app-product-listing',
-  standalone: true,
   imports: [CommonModule, ProductTableComponent],
   templateUrl: './product-listing.component.html',
   styleUrls: ['./product-listing.component.scss'],
 })
 export class ProductListingComponent implements OnInit {
-  constructor() {}
+  products: DisplayedProduct[] = [];
 
-  ngOnInit(): void {}
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next: (response) => {
+        const displayedProducts = response.products.map(
+          ({ id, title, brand, price, stock, rating, thumbnail }) => ({
+            id,
+            title,
+            brand,
+            price,
+            stock,
+            rating,
+            thumbnail,
+          })
+        );
+
+        this.products = displayedProducts;
+      },
+      error: (error) => {
+        console.error('Error fetching products:', error);
+      },
+    });
+  }
 }
