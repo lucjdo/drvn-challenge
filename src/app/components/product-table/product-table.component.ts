c7d77e654bdf40085464aaf381772d21002428ee
lucjdo <luciano.sejudo@gmail.com>
2025-03-31 01:59:48 -0300

    Improve app styling

--- src/app/components/product-table/product-table.component.ts
+++ src/app/components/product-table/product-table.component.ts
@@ -7,11 +7,7 @@ import {
   SimpleChanges,
 } from '@angular/core';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
-import {
-  MatPaginatorModule,
-  MatPaginator,
-  PageEvent,
-} from '@angular/material/paginator';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
 import { DisplayedProduct } from '../../models/product-model';
 import { StockColorDirective } from '../../directives/stock-color.directive';
 import { DecimalPipe, CommonModule } from '@angular/common';
@@ -20,7 +16,6 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { CurrencyService } from '../../services/currency.service';
-import { CurrencySelectorComponent } from '../currency-selector/currency-selector.component';
 
 @Component({
   selector: 'app-product-table',
@@ -34,7 +29,6 @@ import { CurrencySelectorComponent } from '../currency-selector/currency-selecto
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
-    CurrencySelectorComponent,
   ],
   templateUrl: './product-table.component.html',
   styleUrls: ['./product-table.component.scss'],
@@ -61,7 +55,7 @@ export class ProductTableComponent implements OnInit, OnChanges {
     'price',
     'stock',
     'rating',
-    'actions',
+    // 'actions',
   ];
 
   ngOnInit(): void {
@@ -78,7 +72,6 @@ export class ProductTableComponent implements OnInit, OnChanges {
     this.dataSource = new MatTableDataSource(this.products);
     this.dataSource.paginator = this.paginator;
 
-    // Apply existing filter if any
     if (this.filterValue) {
       this.dataSource.filter = this.filterValue.trim().toLowerCase();
     }
@@ -103,6 +96,7 @@ export class ProductTableComponent implements OnInit, OnChanges {
   }
 
   navigateToDetails(id: number): void {
+    console.log('navigateToDetails', id);
     this.router.navigate(['/products', id]);
   }
 }
